8cc7c6b6900b9680216fa7fa301ef466712dd055
daniel <albertagoya@gmail.com>
2023-07-19 09:51:03 +0300

    first commit

---
+++ appuser/Utils/menus.py
@@ -0,0 +1,196 @@
+from appuser.Utils import constants
+
+def response_session_ended():
+    response = ''
+    response = f'END This session has ended. \n'
+
+    return response
+
+
+def response_menu_landing(custom_text_two):
+    # Level 1
+    response = f'CON Welcome to {constants.APP_NAME} \n'
+    response += "1. Accept terms and conditions \n"
+    response += "2. Decline \n"
+    
+    
+    return response
+
+def response_decline_conditions(custom_text_two):
+    response = f'END '
+    response += "Thank you for using the E-Group system \n"
+    return response
+
+def response_menu_one(custom_text_two):
+    # Level 1
+    response = f'CON '
+    response += "1. Savings \n"
+    response += "2. Welfare \n"
+    response += "3. Penalties \n"
+    response += "3. Loans \n"
+    response += "4. Inputs \n"
+    response += "5. Market \n"
+    
+
+    return response
+
+
+# Savings
+
+def response_menu_savings(custom_text_two):
+    response = f'CON '
+    response += "1. Group Savings balance \n"
+    response += "2. Your savings balance \n"
+    response += "3. Do you want to save? \n"
+    return response
+
+def response_menu_group_savings(custom_text_two):
+    response = f'END '
+    response += "Group Savings balance is ksh 10,000 \n"
+    return response
+
+def response_menu_your_savings(custom_text_two):
+    response = f'END '
+    response += "Your Group Savings balance is ksh 3,000 \n"
+    return response
+
+def response_menu_want_to_save(custom_text_two):
+    response = f'CON '
+    response += "1. Yes \n"
+    response += "2. No \n"
+    return response
+
+def response_menu_want_to_save_yes(custom_text_two):
+    response = f'CON '
+    response += "Enter the amount you want to save \n"
+    return response
+
+def response_menu_want_to_save_yes_amount(custom_text_two):
+    response = f'END '
+    response += "An STK push will be sent to your phone \n"
+    return response
+
+def response_menu_want_to_save_no(custom_text_two):
+    response = f'END '
+    response += "Thank you for using the E-Group system \n"
+    return response
+
+# end savings
+
+
+
+# welfare
+
+def response_menu_welfare(custom_text_two):
+    response = f'CON '
+    response += "1. View Welfare group balance \n"
+    response += "2. View Your welfare total savings \n"
+    response += "3. Pay for your welfare savings \n"
+    return response
+
+
+def response_menu_welfare_group_balance(custom_text_two):
+    response = f'END '
+    response += "Welfare group balance is ksh 10,000 \n"
+    return response
+def response_menu_welfare_total_savings(custom_text_two):
+    response = f'END '
+    response += "Your welfare total savings is ksh 10,000 \n"
+    return response
+
+def response_menu_pay_welfare_savings(custom_text_two):
+    response = f'CON '
+    response += "Enter the amount you want to save \n"
+    return response
+
+def response_menu_pay_welfare_savings_stk(custom_text_two):
+    response = f'END '
+    response += "An STK push will be sent to your phone \n"
+    return response
+
+
+# penalties
+
+def response_menu_penalties(custom_text_two):
+    response = f'CON '
+    response += "1. View penalties\n"
+    response += "2. Pay for penalties  \n"
+    return response
+
+
+
+
+def response_main_with_text(text, custom_text, 
+    phone_number, custom_text_two
+    ):
+    
+    # print(f"phone_number2 is {phone_number}")
+
+    level = len(custom_text)
+
+    previous_value = ""
+    print(f"level is {level}")
+    # print(f" {custom_text[-2]}")
+    
+
+    if level == 0:
+        print("is in level 0")
+        return response_menu_landing(custom_text_two)
+
+   
+    if level == 1:
+        print("is in level 1")
+        if custom_text[-1] == '1':
+            return response_menu_one(custom_text)
+        elif custom_text[-1] == '2':
+            return response_decline_conditions(custom_text)
+    
+    elif level == 2:
+        print("levelis 2")
+        if custom_text[-1] == '1':
+            return response_menu_savings(custom_text)
+        if custom_text[-1] == '2':
+            return response_menu_welfare(custom_text)
+        
+    elif level == 3:
+        if custom_text[-2] == '1':
+            if custom_text[-1] == '1':
+                return response_menu_group_savings(custom_text)
+            elif custom_text[-1] == '2':
+                return response_menu_your_savings(custom_text)
+            elif custom_text[-1] == '3':
+                return response_menu_want_to_save(custom_text)
+        elif custom_text[-2] == '2':
+            if custom_text[-1] == '1':
+                return response_menu_welfare_group_balance(custom_text)
+            elif custom_text[-1] == '2':
+                return response_menu_welfare_total_savings(custom_text)
+            elif custom_text[-1] == '3':
+                return response_menu_pay_welfare_savings(custom_text)
+            
+       
+    
+    elif level == 4:
+        if custom_text[-3] == '1':
+            if custom_text[-1] == '1':
+                return response_menu_want_to_save_yes(custom_text)
+            elif custom_text[-1] == '2':
+                return response_menu_want_to_save_no(custom_text)
+        elif custom_text[-3] == '2':
+            return response_menu_pay_welfare_savings_stk(custom_text)
+                
+       
+    elif level == 5:
+        if custom_text[-2] == '1':
+            return response_menu_want_to_save_yes_amount(custom_text)
+        elif custom_text[-2] == '2':
+            return response_menu_pay_welfare_savings_stk(custom_text)
+            
+            
+
+        
+
+   
+   
+    
+        
---
+++ appuser/ussd_views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from appuser.Utils.constants import LEVEL_ONE_OPTIONS_META_DATA, LEVEL_TWO_OPTIONS_META_DATA
+from appuser.Utils.menus import response_main_with_text
+
+from appuser.Utils.utils import goToBack, goToMain, removeUnwantedTexts, replaceCommaSpace, replaceEstericWithSpace, replaceSpaceFromText, saveSessionsToDatabase
+# Create your views here.
+
+
+
+
+
+
+class App_USSD(APIView):
+    def post(self, request, format=None):
+        response = ""
+        # conver request data to JSON, use this outside Africa's talking
+        # data = JSONParser().parse(request)
+
+        data = request.data
+
+        # get params from data, use this with Africa's talking
+        session_id = data.get("SESSION_ID", None)
+        serviceCode = data.get("SERVICE_CODE", None)
+        phone_number = data.get("MSISDN", None)
+        text = data.get("USSD_STRING", None)
+        
+        if text == None:
+            text = data.get("text", "default")
+        
+        networkCode = data.get("networkCode", None)
+
+        # uncomment this when using Africa's talking
+        # session_id = data["sessionId"]
+        # serviceCode = data["serviceCode"]
+        # phone_number = data["phoneNumber"]
+        # text = data["text"]
+        
+        print(f"phone_number is {phone_number}")
+
+        # handle back_texts
+        text = goToBack(text)
+
+        # removing go to main response
+        text = goToMain(text)
+
+        custom_text_two = text
+
+        text = removeUnwantedTexts(
+            array=LEVEL_ONE_OPTIONS_META_DATA["options"], text=text, position_in_text=LEVEL_ONE_OPTIONS_META_DATA["position_in_array"])
+        text = removeUnwantedTexts(
+            array=LEVEL_TWO_OPTIONS_META_DATA["options"], text=text, position_in_text=LEVEL_TWO_OPTIONS_META_DATA["position_in_array"])
+
+     
+
+
+      
+     
+            
+        #Clean the text
+        custom_text = replaceSpaceFromText(text)
+        custom_text = replaceEstericWithSpace(custom_text)
+        custom_text = replaceCommaSpace(custom_text)
+
+        custom_text_two = replaceSpaceFromText(custom_text_two)
+        custom_text_two = replaceEstericWithSpace(custom_text_two)
+
+        custom_text_two = replaceCommaSpace(custom_text_two)
+
+        custom_text = custom_text.split()
+        custom_text_two = custom_text_two.split()
+
+      
+        if text == '' or len(custom_text) == 0:
+            # level 1
+            response = response_main_with_text(
+               text=text, custom_text=custom_text, phone_number=phone_number,
+                custom_text_two=custom_text_two)
+        else:
+            # when the text is not empty
+            response = response_main_with_text(
+                text=text, custom_text=custom_text, phone_number=phone_number,
+                custom_text_two=custom_text_two
+            )
+
+        # Save session here
+
+        # saveSessionsToDatabase(
+        #     phoneNumber=phone_number, text=text, sessionNumber=session_id, response=response)
+
+        return HttpResponse(response)

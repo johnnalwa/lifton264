437bdd06c3b303dce2eb6f6440153a2e28b47a67
daniel <albertagoya@gmail.com>
2023-07-21 16:54:31 +0300

    fixing

--- liffton_project/settings.py
+++ liffton_project/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'liffton_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lifton',
+        'USER': 'admin',
+        'PASSWORD': 'D@t@sc13nc3',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
